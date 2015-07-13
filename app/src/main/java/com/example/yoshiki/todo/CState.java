c2e0fa6a07bd8e06816b2ed63cf6c7f539f9a9e0
sakaki5016 <black.kazoo@gmail.com>
2015-07-13 23:32:41 +0900

    コメントテスト
    テストです

--- app/src/main/java/com/example/yoshiki/todo/CState.java
+++ app/src/main/java/com/example/yoshiki/todo/CState.java
@@ -4,6 +4,7 @@ package com.example.yoshiki.todo;
  * Created by Yoshiki on 2015/07/13.
  */
 public class CState {
+    // コメントを追加してみる（2015.07.13 sakaki）
 
     // アプリケーション状態
     public static final int N_ACTIVITY_CREATE  = 0;    // Todoアイテム作成
